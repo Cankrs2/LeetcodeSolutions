520c75380c0b1869a51b9c0858a5b43a39d0cd8f
Cankrs2 <94618678+Cankrs2@users.noreply.github.com>
2023-12-20 03:56:14 +0300

    Time: 75 ms (6.19%), Space: 17.2 MB (39.00%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,15 +4,13 @@ class Solution:
             ord("a") <= ord(char) <= ord("z") or
             ord("0") <= ord(char) <= ord("9"))
     def isPalindrome(self, s: str) -> bool:
-        left,right = 0 , len(s)-1
-        while left<right:
+        left,right = 0, len(s)-1
+        while left < right:
             while left<right and not self.IsAlphaNumeric(s[left]):
                 left +=1
-            while right >left and not self.IsAlphaNumeric(s[right]):
-                right -=1    
+            while right>left and not self.IsAlphaNumeric(s[right]):
+                right -=1
             if s[left].lower() != s[right].lower():
                 return False
-            left +=1
-            right-=1
+            left,right = left+1,right-1
         return True
-
