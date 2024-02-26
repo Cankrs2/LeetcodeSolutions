85612df43ffa0573fc167eb22917fdd5b09cae86
Cankrs2 <94618678+Cankrs2@users.noreply.github.com>
2024-02-26 11:35:00 +0300

    Time: 42 ms (72.22%), Space: 16.9 MB (72.92%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,40 +1,20 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left,right=0,len(nums)-1
-        while left <= right:
-            mid = left + (right-left)//2
-            if nums[mid] > nums[right]:
-                left = mid+1
-            elif nums[mid] < nums[right]:
-                right = mid
-            else:
-                mid = left
-                break
-                
-        if nums[left] == target:
-            return left
+        left,right = 0,len(nums)-1
         
-        if target > nums[len(nums)-1]:
-            left,right=0,mid
-            while left <= right:
-                mid = left + (right-left)//2
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    right = mid -1
-                elif nums[mid] < target:
-                     left = mid+1
-                        
-        else:
-            left,right=mid,len(nums) -1
-            while left <= right:
-                mid = left + (right-left)//2
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    right = mid -1
-                elif nums[mid] < target:
+        while left<=right:
+            mid = (left+right)//2
+            if target == nums[mid]:
+                return mid
+            
+            if nums[left]<= nums[mid]:
+                if target > nums[mid] or target<nums[left]:
                     left = mid+1
-                
-        return -1          
-                
+                else:
+                    right = mid-1
+            else:
+                if target<nums[mid] or target>nums[right]:
+                    right = mid-1
+                else:
+                    left = mid +1
+        return -1
